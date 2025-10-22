f8bfee3f1c3df1a976598a2d673065be784d8d66
Sanjit Bhattacharjee <237526302+San-code11@users.noreply.github.com>
2025-10-23 00:56:53 +0530

    Helloworld.c , Intro.c
    Codes written during the first programming lab.
    Basic codes on printf function.

---
+++ Lab1/Helloworld.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    // basic programming 
+    // program to print hello world!
+    
+    printf("Hellow world!");
+    
+    return 0;
+    
+    
+}
---
+++ Lab1/Intro.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    //a record of personal informtaion
+    // fields include name and education details
+    
+    printf("Name of student- Sanjit Bhattacharjee \n");
+    
+    printf("Branch-BTech Engineering \n");
+    
+    printf("Roll no- MEB25059 \n");
+    
+    printf("University- Tezpur University");
+    
+    return 0;
+    
+}
